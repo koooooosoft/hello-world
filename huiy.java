f7c5818a46bd72f764495487d70ce13f77c33b85
koooooosoft <54836651+koooooosoft@users.noreply.github.com>
2019-10-08 16:26:01 +0800

    7.5练习

--- huiy.java
+++ huiy.java
@@ -1,23 +1,36 @@
 import java.util.Scanner;
 public class huiy {
 
-	public static void main(String[] args) {
+ 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-	System.out.print("Enter the integers between 1 and 100: ");
-	int [] numbers = new int[101];
-	int a=1;
-	while(a > 0 ){
+	System.out.print("Enter 10 numbers: ");
+	int [] numbers = new int[10];
+	int a;
+	int flag=0;
+	int total=0;
+	for(int i=0;i<10;i++){
 		 a = input.nextInt();
-	     numbers[a]+=1;
-	     if(a==0){ break; }
+		 for(int j=0;j<10;j++){
+			 if(a==numbers[j]){ 
+				 flag=1;
+			 }
+		 }
+	    if(flag==0){
+	    	numbers[i]=a;}
+	    flag=0;
 	  }
-	for(int i=1;i<101;i++){
-		if(numbers[i] == 1) {
-			System.out.println(i + " occurs " + numbers[i] + " time");
+	for(int i=0;i<10;i++){
+		if(numbers[i] > 0) {
+			total+=1;
 		}
-		if(numbers[i] > 1) {
-			System.out.println(i + " occurs " + numbers[i] + " times");
-		}
-	 }
+	}
+	System.out.println("The number of distinct numbers is " + total);
+	System.out.print("The  distinct numbers are " );
+	for(int i=0;i<10;i++){
+		if(numbers[i]>0){
+			System.out.print(numbers[i]+" ");
+			}
+	}
+		
    }
 }
